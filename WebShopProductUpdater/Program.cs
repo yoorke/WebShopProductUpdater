5bdc849596b924d4da7c2819733105793aec404e
yoorke <dejanjurisic@yahoo.com>
2016-09-28 14:25:47 +0200

    webshopProductUpdater

--- WebShopProductUpdater/Program.cs
+++ WebShopProductUpdater/Program.cs
@@ -26,15 +26,26 @@ namespace WebShopProductUpdater
                 XmlDocument data = getXmlData();
                 if(data != null)
                 {
-                    log("Obrađujem XML podatke...");
+                    log("Obrađujem XML podatke...", true);
                     XmlNodeList nodeList = data.DocumentElement.SelectNodes("webdata");
                     int total = nodeList.Count;
-                    int i = 0;
+                    int i = 1;
+                    int status = -1;
+                    int[] statuses = new int[2];
                     foreach (XmlNode xmlNode in nodeList)
                     { 
                         //XmlNode xmlNode = nodeList[0];
-                        updateProduct(xmlNode.SelectSingleNode("barcode").InnerText, xmlNode.SelectSingleNode("naziv") != null ? xmlNode.SelectSingleNode("naziv").InnerText : "none", xmlNode.SelectSingleNode("kol") != null ? xmlNode.SelectSingleNode("kol").InnerText : "-1", xmlNode.SelectSingleNode("pcena").InnerText, total, i++);
+                        status = updateProduct(xmlNode.SelectSingleNode("barcode").InnerText, xmlNode.SelectSingleNode("naziv") != null ? xmlNode.SelectSingleNode("naziv").InnerText : "none", xmlNode.SelectSingleNode("kol") != null ? xmlNode.SelectSingleNode("kol").InnerText : "-1", xmlNode.SelectSingleNode("pcena").InnerText, total, i++);
+                        if (status == 1)
+                            statuses[0]++;
+                        else if (status == 2)
+                            statuses[1]++;
                     }
+
+                    log("Slanje artikala uspešno završeno.");
+                    log("Ukupno dodato: " + statuses[0].ToString());
+                    log("Ukupno ažurirano:" + statuses[1].ToString());
+                    log("Nije pronađeno:" + (nodeList.Count - statuses[0] - statuses[1]).ToString());
                 }
             }
             catch(Exception ex)
@@ -48,7 +59,7 @@ namespace WebShopProductUpdater
         {
             try
             {
-                log("Učitavam XML fajl...");
+                log("Učitavam XML fajl...", true);
                 XmlDocument productsData = new XmlDocument();
                 
 
@@ -85,11 +96,11 @@ namespace WebShopProductUpdater
                 
         //}
 
-        private static void updateProduct(string barcode, string name, string quantity, string price, int total, int i)
+        private static int updateProduct(string barcode, string name, string quantity, string price, int total, int i)
         {
             try
             {
-                log("Šaljem podatke za artikal " + barcode + " (" + i.ToString() + "/" + total.ToString() + ")");
+                log("Šaljem podatke za artikal " + barcode + " (" + i.ToString() + "/" + total.ToString() + ") - ");
                 var request = (HttpWebRequest)WebRequest.Create(ConfigurationManager.AppSettings["webUrl"]);
                 //var request = (HttpWebRequest)WebRequest.Create("http://localhost:63480/api/product");
                 var postData = "barcode=" + barcode;
@@ -110,25 +121,46 @@ namespace WebShopProductUpdater
 
                 var response = (HttpWebResponse)request.GetResponse();
                 var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                int status = int.Parse(responseString);
+                if (status == 1)
+                    log("Uspešno dodat", true);
+                else if (status == 2)
+                    log("Uspešno ažuriran.", true);
+                else if (status == -1)
+                    log("Nije pronađen", true);
+
+                return status;
             }
             catch(Exception ex)
             {
                 log("Greška prilikom slanja podataka" + Environment.NewLine + ex.Message);
                 //Console.WriteLine(ex.Message);
+                return -1;
             }
         }
 
-        private static void log(string message)
+        private static void log(string message, bool newLine = false)
         {
             try
             {
-                Console.WriteLine(message);
+                if (newLine)
+                    Console.WriteLine(message);
+                else
+                    Console.Write(message);
+
                 string filename = string.Format("{0:00}", DateTime.Now.Day) + string.Format("{0:00}", DateTime.Now.Month) + DateTime.Now.Year.ToString() + ".log";
                 using (StreamWriter sw = new StreamWriter("log/" + filename, true, Encoding.GetEncoding(65001)))
                 {
-                    sw.WriteLine(DateTime.Now.ToString());
-                    sw.WriteLine(message);
-                    sw.WriteLine("----------------------------");
+                    sw.Write(DateTime.Now.ToString());
+                    if (newLine)
+                    {                        
+                        sw.WriteLine(" - " + message);
+                    }
+                    else
+                    { 
+                        sw.Write(" - " + message);
+                        //sw.WriteLine("----------------------------");
+                    }
                 }
             }
             catch
